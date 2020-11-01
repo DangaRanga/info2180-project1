79486afe1e1a54df4f8c11daf82edff57739ba21
Jason <jasonblue1626@gmail.com>
2020-11-01 14:55:20 -0500

    Fixed card alignment

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 window.addEventListener('load', function(){
     let submitButton = document.querySelector('.newsletter button');
     let message = document.querySelector('.message');
-    var emailField = document.getElementById('email');
+    const emailField = document.getElementById('email');
     submitButton.addEventListener('click', function(event){
         event.preventDefault();
         let fieldValue = emailField.value;
@@ -10,7 +10,7 @@ window.addEventListener('load', function(){
             message.innerHTML = "Please enter a valid email address";
         }else{
             message.innerHTML = `Thank You! Your email address ${fieldValue} has been added`;
-            document.getElementById('email').value = '';
+            emailField.value = '';
         }
     });
 });
