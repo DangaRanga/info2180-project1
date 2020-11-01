4cb5dc5690137397a75e89e54d1d374522cbe7b9
Jason <jasonblue1626@gmail.com>
2020-11-01 13:29:39 -0500

    fixed form clearing

--- scripts.js
+++ scripts.js
@@ -2,15 +2,15 @@
 window.addEventListener('load', function(){
     let submitButton = document.querySelector('.newsletter button');
     let message = document.querySelector('.message');
-    let emailField = document.getElementById('email');
-    console.log(emailField);
+    var emailField = document.getElementById('email');
     submitButton.addEventListener('click', function(event){
         event.preventDefault();
-        fieldValue = emailField.value;
+        let fieldValue = emailField.value;
         if(fieldValue === ''){
             message.innerHTML = "Please enter a valid email address";
         }else{
             message.innerHTML = `Thank You! Your email address ${fieldValue} has been added`;
+            document.getElementById('email').value = '';
         }
     });
 });
